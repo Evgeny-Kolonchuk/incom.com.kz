ed945b2ba0ef2a29a8cc17a7e28ec9a595f6ccb7
Evgeny <evgeny.kolonchuk@gmail.com>
2022-04-19 14:17:17 +0300

    изменение тайтла

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Нет ВОЙНЕ!");
+$APPLICATION->SetTitle("Нет ВОЙНЕ!!!");
 $GLOBALS["arrFilterMainTheme"] = array("PROPERTY_MAIN_VALUE" => 1);
 $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
 ?>
